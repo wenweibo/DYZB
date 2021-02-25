91217ca64d5a889822aa7752129ee7e240be7c0e
wenweibo <714678701@qq.com>
2021-02-25 17:36:38 +0800

    初始化项目

---
+++ DouYZB/DouYZB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DouYZB
+//
+//  Created by wwb on 2021/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
